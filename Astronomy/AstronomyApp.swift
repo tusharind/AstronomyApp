d5ec3314b817ea4b9d68dab325fcc3e55515acd9
jn <hhj@gmail.com>
2025-12-19 18:21:34 +0530

    Initial Commit

---
+++ Astronomy/AstronomyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstronomyApp.swift
+//  Astronomy
+//
+//  Created by Prakhar Jaiswal on 19/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AstronomyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
